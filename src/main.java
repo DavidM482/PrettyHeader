177a8f10ce0aa1f55376447a1b324d6d3ff702e8
DavidM482 <martin.613901@swl.k12.oh.us>
2024-12-09 08:27:46 -0500

    change

--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        InputHelper.prettyHeader("Pretty Header");
+    }
 }
